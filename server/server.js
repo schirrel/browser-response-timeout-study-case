585da6ce08c8409afc3c68e97ac5ddca772e77f7
Alan Schio <6757777+schirrel@users.noreply.github.com>
2023-07-24 18:35:51 -0300

    add late response code

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ app.post("/post-never-responds", function(request, response) {
   console.log(`this will never reponse`)
 });
 
+
+app.post("/post-late-responds", function(request, response) {
+
+  console.log(`this will never reponse`)
+  setTimeout(()=>{
+      response.send(`/post-late-responds now`)
+  }, 300000)
+});
+
+app.get("/get-late-responds", function(request, response) {
+
+   setTimeout(()=>{
+      response.send(`/get-late-responds now`)
+  }, 300000)
+});
+
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
