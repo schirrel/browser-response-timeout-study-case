cb015dbc41cb7cb27609a7e52014f566967e45c4
Alan Schio <alanbschio@gmail.com>
2023-07-26 11:54:12 -0300

    Adding client and server updates

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
+// server.js
+// where your node app starts
+
+// init project
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
+app.options('*', cors()) 
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
 
   response.send(`/get-never-responds and /post-never-responds`)
 });
 
 app.get("/get-never-responds", function(request, response) {
+
   console.log(`this will never reponse`)
 });
+
 
 app.post("/post-never-responds", function(request, response) {
+
   console.log(`this will never reponse`)
 });
-
 
 app.post("/post-late-responds", function(request, response) {
 
@@ -30,6 +40,7 @@ app.get("/get-late-responds", function(request, response) {
   }, 300000)
 });
 
+// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
